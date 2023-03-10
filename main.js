6eeed6321d8206d7e771f4892f13aeb30d0f9501
Mahmoud elabady <melabady199@gmail.com>
2023-03-10 21:10:25 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,87 @@
+
+function addNewTweet() {
+
+  let newTweetInput = document.getElementById("new-tweet");
+  let newTweetInput2 = document.getElementById("new-tweet2");
+
+  let newTweetText = newTweetInput2.value;
+  let newTweetText2=newTweetInput.value;
+  let input=newTweetText+newTweetText2;
+  if (!newTweetText&&!newTweetText2) {
+    return; 
+  }        
+  const newTweet = document.createElement("div");
+  newTweet.classList.add("feed-tweet");
+  newTweet.innerHTML = `
+    <img src="./Images/MElabady.jpg" class="prof-img">
+    <div class="feed-t-det">
+      <div class="feed-details">
+        <a href="" class="tweeter-name">
+          Mahmoud
+          <div>
+          <span class="tweeter-handle">@m7moud_elabady </span></div>
+          <div><span class="date">. Just now</span></div>
+        </a>
+      </div>
+      <div class="tweet-text">
+        <p>${input}</p>
+      </div>
+      <div class="tweet-icons">
+                    <i class="fa-regular fa-comment"></i>
+                    <i class="fa-solid fa-retweet"></i>
+                  <i class="fa-regular fa-heart"></i>
+                    <i class="fa-solid fa-arrow-up-from-bracket"></i>
+
+                </div>
+    </div>
+  `;
+
+  let tweets = document.querySelector('.tweets');
+  tweets.insertBefore(newTweet, tweets.firstChild);
+  newTweetInput.value="";
+  newTweetInput2.value=""; 
+  document.getElementById("tweetDialog").close();
+
+}
+function showingDialog(){
+const dialog=document.getElementById("tweetDialog");
+dialog.showModal();
+}
+function closingDialog(){
+  const dialog=document.getElementById("tweetDialog");
+  dialog.close();  
+  }
+// function followBtn(){
+//   let followBtn=document.getElementById("follow");
+//   followBtn.innerHTML="<b>Following</b>";
+//   followBtn.style.backgroundColor='rgb(245, 250, 250)';
+//   followBtn.style.color='black';
+//   followBtn.style.border='1px solid gray';
+//   followBtn.style.width='85px';
+// }
+ 
+
+function followBtn(btn){
+  let followBtn=btn
+  if (followBtn.innerHTML === 'Follow') {
+    followBtn.innerHTML="Following";
+    followBtn.className = 'unfollow-btn'
+  } else {
+    followBtn.innerHTML="Follow";
+    followBtn.className = 'follow-btn'
+  }
+}
+function newLine(x){
+  const myTextarea = document.getElementById("new-tweet2");
+  x.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      x.value += '\n';
+      // console.log("Donee");
+    }
+  });
+
+  const btn=document.getElementById("test");
+  btn.style.opacity=1;
+}
+
